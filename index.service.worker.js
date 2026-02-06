81bca41389539f836319c95a5b1f11f5c4ac8450
pizzaspren <comma-ended-speech@duck.com>
2026-02-07 00:01:18 +0100

    alpha-0.0.8

--- index.service.worker.js
+++ index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1770302507|15511561246';
+const CACHE_VERSION = '1770418830|5911823380';
 /** @type {string} */
 const CACHE_PREFIX = 'Potato Plandoer-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;

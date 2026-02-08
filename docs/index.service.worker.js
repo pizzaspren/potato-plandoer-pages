74f24c8d8365f81cb31d4b9830a0e883c2b7d659
pizzaspren <comma-ended-speech@duck.com>
2026-02-08 12:36:57 +0100

    Walls? : alpha-0.0.11

--- docs/index.service.worker.js
+++ docs/index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1770477124|301472214';
+const CACHE_VERSION = '1770550585|4527786298';
 /** @type {string} */
 const CACHE_PREFIX = 'Potato Plandoer-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;

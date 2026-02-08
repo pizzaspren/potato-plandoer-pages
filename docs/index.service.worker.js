b10bec86a1f2d3951f59ef85c869226669db11c4
pizzaspren <comma-ended-speech@duck.com>
2026-02-08 19:16:12 +0100

    Fix v4 walls : alpha-0.0.12

--- docs/index.service.worker.js
+++ docs/index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1770550844|4786643367';
+const CACHE_VERSION = '1770574520|114184552';
 /** @type {string} */
 const CACHE_PREFIX = 'Potato Plandoer-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;

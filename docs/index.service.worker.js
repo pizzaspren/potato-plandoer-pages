661e6719bd90001dda3610b86a49892ad5d22c40
pizzaspren <comma-ended-speech@duck.com>
2026-02-15 23:21:01 +0100

    Fix pickup removal : alpha-0.0.17

--- docs/index.service.worker.js
+++ docs/index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1771192547|1048515958';
+const CACHE_VERSION = '1771194044|2545803940';
 /** @type {string} */
 const CACHE_PREFIX = 'Potato Plandoer-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;

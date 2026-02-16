996e00f6c18af6ba1fae4adbe02e0ffedfb7e4da
pizzaspren <comma-ended-speech@duck.com>
2026-02-16 19:37:19 +0100

    Load pickups from json : alpha-0.1.0

--- docs/index.service.worker.js
+++ docs/index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1771260913|6494662101';
+const CACHE_VERSION = '1771267025|3177825903';
 /** @type {string} */
 const CACHE_PREFIX = 'Potato Plandoer-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
